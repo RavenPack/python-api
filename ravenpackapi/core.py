0d55a1660085895f853452d921a2a212ffeddb94
Dario Varotto <dariosky@gmail.com>
2021-07-20 11:20:04 +0200

    version 1.0.50

--- ravenpackapi/core.py
+++ ravenpackapi/core.py
@@ -14,7 +14,7 @@ from ravenpackapi.utils.date_formats import as_datetime_str
 from ravenpackapi.utils.dynamic_sessions import DynamicSession
 
 _VALID_METHODS = ('get', 'post', 'put', 'delete', 'patch')
-VERSION = '1.0.49'
+VERSION = '1.0.50'
 
 logger = logging.getLogger("ravenpack.core")
 
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '1.0.49'
+VERSION = '1.0.50'
 
 with open('README.rst') as readme_file:
     readme = readme_file.read()
