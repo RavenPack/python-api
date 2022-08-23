0f8cfa41664c9658ca55913112a675c20506d42f
Jose Ramon Cano Yribarren <jcano@ravenpack.com>
2022-08-23 15:37:32 +0200

    Refactor example code

--- ravenpackapi/examples/get_historical_flat_files.py
+++ ravenpackapi/examples/get_historical_flat_files.py
@@ -7,32 +7,62 @@ import zipfile
 from ravenpackapi import RPApi
 from ravenpackapi.util import parse_csv_line
 
-api_key = os.environ['RP_API_KEY']  # set your API KEY here
-api = RPApi(api_key)
 
-flat_type = 'companies'  # can be 'companies' or 'full'
-flat_list = api.get_flatfile_list(flat_type)
-for flat_file in flat_list:
-    file_id = flat_file['id']
-    combined_year_filename = '%s.combined.csv' % file_id
-    if not os.path.isfile(combined_year_filename):
-        with open(combined_year_filename, 'wb') as output:
-            headers_written = False
-            with api.get_flatfile(flat_type, file_id) as flatzip:
-                if not os.path.isfile(file_id):
-                    print("Downloading", file_id, flat_file['size'] / 1024 / 1024, "MB")
-                    with open(file_id, 'wb') as f:
-                        for chunk in flatzip.iter_content(chunk_size=8192):
-                            f.write(chunk)
-                with zipfile.ZipFile(file_id) as zipped:
-                    for fileinfo in zipped.namelist():
-                        print(fileinfo)
-                        with zipped.open(fileinfo) as csv:
-                            header_line = next(csv)
-                            headers = parse_csv_line(header_line)
-                            if not headers_written:
-                                output.write(header_line)
-                                headers_written = True
-                            for line in csv:
-                                row = parse_csv_line(line)
-                                output.write(line)
+def main():
+    api_key = os.environ["RP_API_KEY"]  # set your API KEY here
+    api = RPApi(api_key)
+
+    flat_type = "companies"  # can be 'companies' or 'full'
+    flat_list = api.get_flatfile_list(flat_type)
+
+    for flat_file in flat_list:
+        file_id = flat_file["id"]
+        combined_year_filename = "%s.combined.csv" % file_id
+        if os.path.isfile(combined_year_filename):
+            continue
+        download_flatfile(api, flat_type, flat_file)
+        unzip_to_csv(file_id, combined_year_filename)
+
+
+def unzip_to_csv(zipname: str, csvname: str):
+    with open(csvname, "wb") as output:
+        with zipfile.ZipFile(zipname) as zf:
+            for line in get_all_csv_lines_in_zip(zf):
+                headers = parse_csv_line(line)
+                output.write(line)
+
+
+def get_all_csv_lines_in_zip(zf: zipfile.ZipFile):
+    headers_written = False
+    for fileinfo in zf.namelist():
+        print(fileinfo)
+        with zf.open(fileinfo) as csv:
+            header_line = next(csv)
+            if not headers_written:
+                yield header_line
+                headers_written = True
+            for line in csv:
+                yield line
+
+
+def download_flatfile(
+    api: RPApi, flat_type: str, flat_file: dict, chunk_size: int = 8192
+):
+    filename = flat_file["id"]
+    flatsize = flat_file["size"]
+    if os.path.isfile(filename):
+        return False
+    with api.get_flatfile(flat_type, filename) as flatzip:
+        print("Downloading", filename, bytes_to_mbytes(flatsize), "MB")
+        with open(filename, "wb") as f:
+            for chunk in flatzip.iter_content(chunk_size=chunk_size):
+                f.write(chunk)
+    return True
+
+
+def bytes_to_mbytes(b: int):
+    return b / 1024 / 1024
+
+
+if __name__ == "__main__":
+    main()
