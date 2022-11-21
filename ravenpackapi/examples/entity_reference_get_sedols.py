2ff86ab2f92b28cb1aff460b4b76421289096f1d
Jose Ramon Cano Yribarren <jcano@ravenpack.com>
2022-11-21 14:35:00 +0100

    Rewrite example

--- ravenpackapi/examples/entity_reference_get_sedols.py
+++ ravenpackapi/examples/entity_reference_get_sedols.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 
 from ravenpackapi import RPApi
 
@@ -7,23 +8,34 @@ from ravenpackapi import RPApi
 # like in the query_entity_reference.py example
 api = RPApi()
 
-reference_filename = 'reference.csv'
-if os.path.isfile(reference_filename):
-    # use the locally saved reference file if it exists
-    reference = api.get_entity_type_reference_from_file(reference_filename)
-else:
-    print("Retrieving the company mapping file")
-    # get the latest reference file for all the COMP entities
-    # call it without arguments to get all entities of all types
-    reference = api.get_entity_type_reference('COMP')
-    reference.write_to_file(reference_filename)
 
-# with the reference we can also ask for a single entity given the ID
-for rp_entity_id in ['4A6F00', '01F2E5']:  # add here as many as you want - they won't cause extra requests
-    company = reference[rp_entity_id]
-    valid_sedols = [sedol.value
-                    for sedol in company.sedols
-                    if sedol.is_valid()  # get all the sedol that are valid now
-                    # (you can pass a date to is_valid to get the ones valid point-in-time
-                    ]
-    print(company.name, valid_sedols)
+def download_or_read_reference_file(reference_filename="reference.csv"):
+    if os.path.isfile(reference_filename):
+        # use the locally saved reference file if it exists
+        reference = api.get_entity_type_reference_from_file(reference_filename)
+    else:
+        print("Retrieving the company mapping file")
+        # get the latest reference file for all the COMP entities
+        # call it without arguments to get all entities of all types
+        reference = api.get_entity_type_reference("COMP")
+        reference.write_to_file(reference_filename)
+    return reference
+
+
+def get_valid_sedols(company, when=None):
+    """Get all the sedol that are valid now (you can pass a date to is_valid to get the ones valid point-in-time"""
+    return [sedol.value for sedol in company.sedols if sedol.is_valid(when)]
+
+
+if __name__ == "__main__":
+    # with the reference we can also ask for a single entity given the ID
+    reference = download_or_read_reference_file()
+    NOW = None
+    entities = [
+        ("4A6F00", NOW),
+        ("01F2E5", datetime.date(2019, 1, 1)),
+    ]
+    for rp_entity_id, when in entities:  # add here as many as you want - they won't cause extra requests
+        company = reference[rp_entity_id]
+        valid_sedols = get_valid_sedols(company, when)
+        print(company.name, valid_sedols)
