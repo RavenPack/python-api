127946d5e9a2458dad812ddfea2859930cc502f9
Jose Ramon Cano Yribarren <jcano@ravenpack.com>
2022-11-21 16:34:06 +0100

    Adapt script to work in Edge

--- ravenpackapi/examples/entity_reference_get_sedols.py
+++ ravenpackapi/examples/entity_reference_get_sedols.py
@@ -3,10 +3,7 @@ import datetime
 
 from ravenpackapi import RPApi
 
-# With Edge - the Reference-mapping are huge - so they're not kept in memory
-# you can either save them to file and filter them or query individual entities
-# like in the query_entity_reference.py example
-api = RPApi()
+api = RPApi(product="rpa")  # Or product="edge"
 
 
 def download_or_read_reference_file(reference_filename="reference.csv"):
@@ -29,13 +26,23 @@ def get_valid_sedols(company, when=None):
 
 if __name__ == "__main__":
     # with the reference we can also ask for a single entity given the ID
-    reference = download_or_read_reference_file()
     NOW = None
     entities = [
         ("4A6F00", NOW),
         ("01F2E5", datetime.date(2019, 1, 1)),
     ]
-    for rp_entity_id, when in entities:  # add here as many as you want - they won't cause extra requests
-        company = reference[rp_entity_id]
+    if api.product == "edge":
+        # With Edge - the Reference-mapping are huge - so they're not kept in memory
+        # you can either save them to file (about 2GB) and filter them or query
+        # individual entities like this:
+        reference = {
+            entity_key: api.get_entity_reference(entity_key)
+            for entity_key, date in entities
+        }
+    else:
+        reference = download_or_read_reference_file()
+
+    for entity_key, when, in entities:
+        company = reference[entity_key]
         valid_sedols = get_valid_sedols(company, when)
         print(company.name, valid_sedols)
