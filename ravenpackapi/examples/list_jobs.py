711a5d11a61f0766bda794dac2c8af12edef0675
Jose Ramon Cano Yribarren <jcano@ravenpack.com>
2022-11-22 17:51:10 +0100

    Enrich example list_jobs.py with more info

--- ravenpackapi/examples/list_jobs.py
+++ ravenpackapi/examples/list_jobs.py
@@ -1,7 +1,46 @@
+"""
+Example that lists jobs (processing, completed or in error) that have been
+updated in a time range, and prints them.
+"""
+
+import datetime
 from ravenpackapi import RPApi
 
 # initialize the API (here we use the RP_API_KEY in os.environ)
-api = RPApi()
+PRODUCT = "rpa"  # Or PRODUCT = "edge"
+api = RPApi(product=PRODUCT)
 
-for job in api.list_jobs('2020-01-01', '2020-10-10', status=['COMPLETED']):
-    print(job)
+
+def print_jobs(jobs):
+    for job in jobs:
+        print_job(job)
+
+
+def print_job(job):
+    status = "failed" if job.status == "error" else job.status
+    start, end = [job._data[date] for date in ("start_date", "end_date")]
+    id = job.token
+    print(f"Job {status} with ID {id} with data ranging from {start} to {end}")
+
+
+if __name__ == "__main__":
+    # Define start_date and end_date or use the current date
+    # start_date = "2022-11-22"
+    # end_date = "2022-11-23"
+    DAYS_BACK = 1
+    end_date = datetime.datetime.utcnow().replace(microsecond=0)
+    start_date = end_date - datetime.timedelta(days=DAYS_BACK)
+
+    # Print all finished_jobs: completed or in error
+    finished_jobs = api.list_jobs(start_date, end_date, status=["completed", "error"])
+    print(
+        f"{len(finished_jobs)} jobs completed or in error between {start_date} and {end_date}:"
+    )
+    print_jobs(finished_jobs)
+
+    # Print running jobs
+    jobs = api.list_jobs(start_date, end_date, status=["processing"])
+    print(
+        f"{len(jobs)} jobs processing between {start_date} and {end_date} with tokens:"
+    )
+    print_jobs(jobs)
