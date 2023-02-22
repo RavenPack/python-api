0b4bc918a404154f45b7cc5bb3f74a2b105c93e8
Jose Ramon Cano Yribarren <jcano@ravenpack.com>
2023-02-22 12:28:57 +0100

    Add example to delete a dataset

---
+++ ravenpackapi/examples/delete_dataset.py
@@ -0,0 +1,21 @@
+from ravenpackapi import RPApi, Dataset
+from ravenpackapi.exceptions import APIException404
+
+DATASET_ID = "THE_ID_OF_THE_DATASET_TO_DOWNLOAD"
+
+
+def main():
+    api = RPApi()
+    ds = Dataset(api=api, id=DATASET_ID)
+
+    try:
+        do_delete = input(f"Are you sure you want to delete the {ds}? (y/N)")
+        if do_delete.upper() == "Y":
+            ds.delete()
+            print("Deleted", ds)
+    except APIException404:
+        print("Can't delete dataset because it was not found")
+
+
+if __name__ == "__main__":
+    main()
